1d771f57e43a111ee7b2b2df1f74684c0ec188bc
Allison Kuhl <allison.kuhl@bell.net>
2018-09-17 12:15:56 -0400

    seperated deck length test from shuffle test

--- src/test/java/core/DeckTest.java
+++ src/test/java/core/DeckTest.java
@@ -8,12 +8,20 @@ public class DeckTest extends TestCase {
 	
 	@Test
 	//tests to see if a randomized deck of cards is created properly
-	public void testDeck() {
+	public void testDeckLength() {
 		Deck testDeck = new Deck();
 		
 		//make sure deck of cards contains total of 52 cards
 		assertTrue(testDeck.getLength() == 52);
 		
+		
+		
+	}
+	
+	@Test
+	public void testDeckShuffle() {
+		
+		Deck testDeck = new Deck();
 		//see if it contains certain cards
 		assertTrue(testDeck.contains("HA"));
 		assertTrue(testDeck.contains("C5"));
@@ -21,7 +29,6 @@ public class DeckTest extends TestCase {
 		//make sure it is randomized (i.e. if another is created it won't be the same order)
 		Deck testDeck2 = new Deck();
 		assertFalse(testDeck.isSameAs(testDeck2));
-		
 	}
 	
 	@Test 
