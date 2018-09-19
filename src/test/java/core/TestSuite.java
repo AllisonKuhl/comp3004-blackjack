67c61f80b1d4af7c7bd56281c60a7ba14a26be1b
Allison Kuhl <allison.kuhl@bell.net>
2018-09-19 17:14:38 -0400

    added test suite

---
+++ src/test/java/core/TestSuite.java
@@ -0,0 +1,12 @@
+package core;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({GameControllerTest.class, GameTest.class, DeckTest.class, SplitTest.class})
+public class TestSuite {
+  //nothing
+}
+
+
